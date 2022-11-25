6308a8d6c1e8a3ecca3d038722308257a22fb3c5
yannikkellerde <yannik@kelnet.de>
2022-11-25 16:58:35 +0100

    Trying something smart

---
+++ cpp_graph_game/hex_graph_game/shannon_restructured.cpp
@@ -0,0 +1,599 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <utility>
+#include <numeric>
+#include <algorithm>
+#include <chrono>
+#include <math.h>
+#include "shannon_restructured.h"
+#include "util.h"
+
+
+bool check_if_same(Graph& graph, int v1, int v2){
+	Neighbors neigh1 = graph.adjacent_vertices(v1);
+	Neighbors neigh2 = graph.adjacent_vertices(v2);
+	if (neigh1.second-neigh1.first!=neigh2.second-neigh2.first){
+		return false;
+	}
+	for (;neigh1.first!=neigh1.second;++neigh1.first){
+		int n1 = *neigh1.first;
+		if (n1 != v2){
+			vector<int>::const_iterator pi = neigh2.first;
+			bool found = false;
+			for (;pi!=neigh2.second;++pi){
+				if (*pi==n1){
+					found = true;
+					break;
+				}
+			}
+			if (!found){
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool is_fully_connected(Graph& g,int vert, int ignore){
+	Neighbors neigh = g.adjacent_vertices(vert);
+	for (;neigh.first!=neigh.second;++neigh.first){
+		int v1 = *neigh.first;
+		if (v1!=ignore){
+			for (vector<int>::const_iterator it2 = neigh.first+1;it2!=neigh.second;++it2){
+				int v2 = *it2;
+				if (v2!=ignore && !g.edge_exists(v1,v2)){
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+Node_switching_game::Node_switching_game (int board_size):board_size(board_size),board(board_size){
+	reset();
+};
+
+Node_switching_game::Node_switching_game (std::vector<torch::Tensor> &data){
+	// Convert model repr back to graph
+	torch::Tensor node_features = data[0].cpu();
+	torch::Tensor edge_index = data[1].cpu();
+	graph = Graph(node_features.size(0));
+	graph.add_fprop(0.); // IS_TERMINAL
+	graph.add_lprop(0); // REMOVED
+	graph.add_lprop(0); // BOARD_LOCATION
+	graph.fprops[IS_TERMINAL][TERMINAL_1] = 1;
+	graph.fprops[IS_TERMINAL][TERMINAL_2] = 1;
+	for (int i=0;i<graph.num_vertices;i++){
+		graph.lprops[BOARD_LOCATION][i] = i;
+	}
+	for (int i=0;i<edge_index.size(1);i++){
+		long source = edge_index[0][i].item<long>();
+		long target = edge_index[1][i].item<long>();
+		graph.add_edge(source,target);
+	}
+}
+Node_switching_game::Node_switching_game (Hex_board& from_board):board(from_board){
+	reset();
+};
+
+Node_switching_game::Node_switching_game (Node_switching_game& ref){
+#ifndef SINGLE_GRAPH
+	graph2 = ref.graph2;
+#endif
+	graph = ref.graph;
+#ifndef NO_PLAY
+	response_set_red = ref.response_set_red;
+	response_set_blue = ref.response_set_blue;
+	board_moves_red = ref.board_moves_red;
+	board_moves_blue = ref.board_moves_blue;
+#endif
+	onturn = ref.onturn;
+	board_size = ref.board_size;
+	board = ref.board;
+	move_num = ref.move_num;
+}
+
+Node_switching_game * Node_switching_game::clone(){
+	return new Node_switching_game(*this);
+}
+
+void Node_switching_game::reset(){
+	move_num = 0;
+	board_size = board.size;
+#ifndef NO_PLAY
+	response_set_red = map<int,int>();
+	response_set_blue = map<int,int>();
+	board_moves_red = vector<int>();
+	board_moves_blue = vector<int>();
+#endif
+	onturn = RED;
+	auto f = [this](Graph& g) {
+		g = Graph(board.num_squares);
+		g.add_fprop(0.); // IS_TERMINAL
+		g.add_lprop(0); // REMOVED
+		g.add_lprop(0); // BOARD_LOCATION
+		g.fprops[IS_TERMINAL][TERMINAL_1] = 1;
+		g.fprops[IS_TERMINAL][TERMINAL_2] = 1;
+	};
+	f(graph);
+	for (int i=0;i<board.num_squares;i++){
+		graph.lprops[BOARD_LOCATION][i] = i;
+		if (i<board.size){
+			graph.add_edge(i,TERMINAL_1);
+		}
+		if (floor(i/board.size)==board.size-1){
+			graph.add_edge(i,TERMINAL_2);
+		}
+		if (i%board.size>0 && board.size<=i && i<=board.num_squares-board.size){
+			graph.add_edge(i,i-1);
+		}
+		if (i>=board.size){
+			graph.add_edge(i,i-board.size);
+			if (i%board.size!=board.size-1){
+				graph.add_edge(i,i+1-board.size);
+			}
+		}
+	}
+#ifndef SINGLE_GRAPH
+	f(graph2);
+	for (int i=0;i<board.num_squares;i++){
+		graph2.lprops[BOARD_LOCATION][i] = i;
+		if (i%board.size==0){
+			graph.add_edge(i,TERMINAL_1);
+		}
+		if (i%board.size==board.size-1){
+			graph.add_edge(i,TERMINAL_2);
+		}
+		if (i%board.size>0 && board.size<=i && i<=board.num_squares-board.size){
+			graph.add_edge(i,i-1);
+		}
+		if (i>=board.size){
+			graph.add_edge(i,i-board.size);
+		}
+	}
+#endif
+}
+
+uint32_t Node_switching_game::hash_key() const { // https://stackoverflow.com/a/27216842
+	std::size_t seed = graph.sources.size();
+#ifdef SINGLE_GRAPH
+	for (const Graph& g:{graph}){
+#else
+	for (const Graph& g:{graph,graph2}){
+#endif
+		for(const int& i : graph.sources) {
+			seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		}
+		for(const int& i : graph.targets) {
+			seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		}
+	}
+	return seed;
+}
+
+int Node_switching_game::vertex_from_board_location(int bl) const{
+	int i=0;
+	for (vector<int>::const_iterator vs = graph.lprops[BOARD_LOCATION].begin();vs!=graph.lprops[BOARD_LOCATION].end();++vs,++i){
+		int v = *vs;
+		if (v == bl){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int Node_switching_game::action_from_board_location(int bl) const{
+	return vertex_from_board_location(bl)-2;
+}
+
+#ifndef NO_PLAY
+int Node_switching_game::get_response(int bloc,bool for_red){
+	if (response_set_red.find(bloc)!=response_set_red.end()){
+		if (for_red){
+			return response_set_red[bloc];
+		}
+		else{
+			int otherside = response_set_red[bloc];
+			response_set_red.erase(otherside);
+			response_set_red.erase(bloc);
+			return -1;
+		}
+	}
+	else if (response_set_blue.find(bloc)!=response_set_blue.end()){
+		if (!for_red){
+			return response_set_blue[bloc];
+		}
+		else{
+			int otherside = response_set_blue[bloc];
+			response_set_blue.erase(otherside);
+			response_set_blue.erase(bloc);
+			return -1;
+		}
+	}
+	return -1;
+}
+#endif
+
+Onturn Node_switching_game::not_onturn(){
+	return onturn==RED?BLUE:RED;
+}
+
+void Node_switching_game::switch_onturn(){
+	onturn = onturn==RED?BLUE:RED;
+}
+
+set<int> Node_switching_game::fix_terminal_connections(int terminal){
+	set<int> change_set;
+	auto f = [this,terminal](Graph& g,set<int>& change_set) {
+		vector<pair<int,int>> to_del;
+		for (Neighbors neigh = g.adjacent_vertices(terminal);neigh.first!=neigh.second;++neigh.first){
+			int v1 = *neigh.first;
+			for (vector<int>::const_iterator neigh2 = neigh.first+1;neigh2!=neigh.second;++neigh2){
+				int v2 = *neigh2;
+				if (v2!=v1 && g.edge_exists(v1,v2)){
+					to_del.push_back(pair<int,int>(v1,v2));
+					change_set.insert(v1);
+					change_set.insert(v2);
+				}
+			}
+		}
+		for (pair<int,int>del_me:to_del){
+			g.delete_edge(del_me.first,del_me.second);
+		}
+	};
+	f(graph,change_set);
+#ifndef SINGLE_GRAPH
+	f(graph2,change_set);
+#endif
+
+	return change_set;
+}
+
+void Node_switching_game::remove_marked_nodes(){
+	auto f = [this](Graph& g) {
+		vector<int>::iterator rem = g.lprops[REMOVED].end()-1;
+		for (int i=g.num_vertices-1;i>=0;--i,--rem){
+			if (*rem){
+				g.remove_vertex(i);
+			}
+		}
+	};
+	f(graph);
+#ifndef SINGLE_GRAPH
+	f(graph2);
+#endif
+}
+
+int Node_switching_game::get_random_action() const{
+	vector<int> actions = get_actions();
+	return repeatable_random_choice(actions);
+}
+
+vector<int> Node_switching_game::get_actions() const{
+	vector<int> res;
+	if (swap_allowed&&move_num==1){ // allow swap
+		vector <int> res(graph.num_vertices-1);
+	}
+	else{
+		vector <int> res(graph.num_vertices-2);
+	}
+	iota(res.begin(),res.end(),0);
+	return res;
+}
+
+
+set<int> Node_switching_game::make_move(int action, bool do_force_color, Onturn force_color,bool do_remove_dead_and_captured,bool only_mark_removed){
+	set<int> change_set;
+	int vertex = action+2;
+	Onturn player = do_force_color?force_color:onturn;
+	if (!do_force_color){
+		++move_num;
+		switch_onturn();
+	}
+	if (swap_allowed&&vertex == graph.num_vertices&&move_num==2){ // swap rule
+		swap(graph,graph2);
+		return change_set;
+	}
+	assert(vertex<graph.num_vertices);
+	auto f = [this,player,vertex,do_remove_dead_and_captured,only_mark_removed](Graph& g, Onturn maker_color) {
+		set<int> change_set;
+#ifndef NO_PLAY
+		if (player == RED){
+			board_moves_red.push_back(g.lprops[BOARD_LOCATION][vertex]);
+		}
+		else{
+			board_moves_blue.push_back(g.lprops[BOARD_LOCATION][vertex]);
+		}
+#endif
+		if (player==maker_color){
+			vector<pair<int,int>> to_add;
+			int have_to_fix=-1;
+			for (Neighbors neigh = g.adjacent_vertices(vertex);neigh.first!=neigh.second;++neigh.first){
+				int v1 = *neigh.first;
+				if (v1<2){
+					have_to_fix = v1;
+				}
+				for (vector<int>::const_iterator it2 = neigh.first+1;it2!=neigh.second;++it2){
+					int v2 = *it2;
+					if (v2<2){
+						have_to_fix = v2;
+					}
+					if ((!((g.edge_exists(v1,TERMINAL_1))&&(g.edge_exists(v2,TERMINAL_1))))&&(!((g.edge_exists(v1,TERMINAL_2))&&(g.edge_exists(v2,TERMINAL_2)))))
+						to_add.push_back(pair<int,int>(v1,v2));
+					}
+				}
+			for (pair<int,int> add_me : to_add){
+				g.add_edge(add_me.first,add_me.second);
+			}
+			if (have_to_fix!=-1){
+				change_set = fix_terminal_connections(have_to_fix);
+			}
+		}
+		if (do_remove_dead_and_captured){
+			Neighbors neigh = g.adjacent_vertices(vertex);
+			change_set.insert(neigh.first,neigh.second);
+		}
+		g.clear_vertex(vertex);
+		if (do_remove_dead_and_captured || only_mark_removed){
+			g.lprops[REMOVED][vertex] = 1;
+		}
+		else{
+			g.remove_vertex(vertex);
+		}
+		return change_set;
+	};
+	change_set = f(graph,RED);
+#ifndef SINGLE_GRAPH
+	f(graph2,BLUE);
+#endif
+	if (do_remove_dead_and_captured){
+		remove_dead_and_captured(change_set);
+		remove_marked_nodes();
+	}
+	return change_set;
+}
+
+void Node_switching_game::remove_dead_and_captured(set<int> &consider_set){
+	bool can_continue,is_dead;
+	int v1;
+	set<int> big_set, change_set;
+	Neighbors neigh_neigh,tmp_neigh,neighbors,neigh_neigh_neigh;
+	for (int vertex : consider_set){
+		is_dead = true;
+		can_continue = false;
+		if (graph.lprops[REMOVED][vertex]||vertex<2){
+			continue;
+		}
+		/* cout << "considering " << vertex << endl; */
+		neighbors = graph.adjacent_vertices(vertex);
+
+		if (neighbors.first!=neighbors.second){
+			neigh_neigh = graph.adjacent_vertices(*neighbors.first);
+			for (;neigh_neigh.first<=neigh_neigh.second;++neigh_neigh.first){
+				if (neigh_neigh.first==neigh_neigh.second){
+					v1 = *neighbors.first;
+				}
+				else{
+					v1 = *neigh_neigh.first;
+				}
+				if (v1==vertex||v1<2){
+					continue;
+				}
+				if (check_if_same(graph,v1,vertex)){
+#ifndef NO_PLAY
+					response_set_red[graph.lprops[BOARD_LOCATION][v1]]=graph.lprops[BOARD_LOCATION][vertex];
+					response_set_red[graph.lprops[BOARD_LOCATION][vertex]]=graph.lprops[BOARD_LOCATION][v1];
+#endif
+					/* tmp_neigh = adjacent_vertices(vertex,graph);       // These are not */
+					/* big_set.insert(tmp_neigh.first,tmp_neigh.second);  // in the python original */
+					/* cout << "maker captured " << v1 << vertex << endl; */
+					neigh_neigh_neigh = graph.adjacent_vertices(v1);
+					big_set.insert(neigh_neigh_neigh.first,neigh_neigh_neigh.second);
+					change_set = make_move(v1,true,BLUE,false,true);
+					change_set = make_move(vertex,true,RED,false,true);
+					big_set.insert(change_set.begin(),change_set.end());
+					can_continue = true;
+					break;
+				}
+			}
+		}
+		if (can_continue){
+			continue;
+		}
+
+		for (neighbors = graph.adjacent_vertices(vertex);neighbors.first!=neighbors.second;++neighbors.first){
+			v1 = *neighbors.first;
+			for (vector<int>::const_iterator it2 = neighbors.first+1;it2!=neighbors.second;++it2){
+				if (is_dead&&!graph.edge_exists(*neighbors.first,*it2)){
+					is_dead = false;
+				}
+			}
+			if (is_fully_connected(graph,vertex,v1) && is_fully_connected(graph,v1,vertex)){
+#ifndef NO_PLAY
+				response_set_blue[graph.lprops[BOARD_LOCATION][v1]]=graph.lprops[BOARD_LOCATION][vertex];
+				response_set_blue[graph.lprops[BOARD_LOCATION][vertex]]=graph.lprops[BOARD_LOCATION][v1];
+				board_moves_blue.push_back(graph.lprops[BOARD_LOCATION][v1]);
+				board_moves_blue.push_back(graph.lprops[BOARD_LOCATION][vertex]);
+#endif
+				/* cout << "breaker captured " << v1 << " " << vertex << endl; */
+				tmp_neigh = graph.adjacent_vertices(vertex);
+				neigh_neigh = graph.adjacent_vertices(v1);
+				big_set.insert(tmp_neigh.first,tmp_neigh.second);
+				big_set.insert(neigh_neigh.first,neigh_neigh.second);
+				change_set = make_move(v1,true,BLUE,false,true);
+				change_set = make_move(vertex,true,BLUE,false,true);
+				can_continue = true;
+				break;
+			}
+		}
+		if (is_dead && !can_continue){
+#ifndef NO_PLAY
+			board_moves_blue.push_back(graph.lprops[BOARD_LOCATION][vertex]);
+#endif
+			/* cout << "node is dead " << vertex << endl; */
+			neighbors = graph.adjacent_vertices(vertex);
+			big_set.insert(neighbors.first,neighbors.second);
+			change_set = make_move(vertex,true,BLUE,false,true);
+			continue;
+		}
+	}
+	if (!big_set.empty()){
+		remove_dead_and_captured(big_set);
+	}
+}
+
+TerminalType Node_switching_game::get_winner() const{
+	Onturn res = who_won();
+	if (res==onturn){ // This is from perspective of current onturn player
+		return TERMINAL_WIN;
+	}
+	else if (res==NOPLAYER){
+		return TERMINAL_NONE;
+	}
+	else{
+		return TERMINAL_LOSS;
+	}
+
+}
+
+Onturn Node_switching_game::who_won() const{
+	bool found1,found2,not_again;
+	if (graph.edge_exists(TERMINAL_1,TERMINAL_2)){
+		return RED;
+	}
+#ifdef SINGLE_GRAPH
+	bool visited[graph.num_vertices];
+	int src=TERMINAL_1;
+	fill(visited,visited+graph.num_vertices,false);
+	visited[src] = true;
+	std::stack<int> next;
+	next.push(src);
+
+	while(!next.empty()) {
+		int cv = next.top();
+		next.pop();
+		for (Neighbors neigh = graph.adjacent_vertices(cv);neigh.first!=neigh.second;++neigh.first){
+			int nv = *neigh.first;
+			if (!visited[nv]){
+				if (nv == TERMINAL_2){
+					return NOPLAYER;
+				}
+					visited[nv] = true;
+					next.push(nv);
+				}
+			}
+		}
+	return BLUE;
+#else
+	if (graph2.edge_exists(TERMINAL_1,TERMINAL_2)){
+		return BLUE;
+	}
+	return NOPLAYER;
+#endif
+}
+
+vector<string> Node_switching_game::get_grid_layout() const{
+	double scale;
+	vector<string> position_array(graph.num_vertices);
+	scale = 1+0.1*(11-board_size);
+	const double xstart = 0;
+	const double ystart = 0;
+	const double xend = xstart+1.5*(board_size-1)*scale;
+	const double yend = ystart+sqrt(3./4.)*(board_size-1)*scale;
+	for (int i=0;i<graph.num_vertices;i++){
+		int bi = graph.lprops[BOARD_LOCATION][i];
+		int row = floor(bi/board_size);
+		int col = bi%board_size;
+		position_array[i] = to_string((xstart+(0.5*col+row)*scale))+","+to_string((ystart + (sqrt(3./4.)*col)*scale))+"!";
+	}
+	return position_array;
+}
+
+vector<string> Node_switching_game::get_colors() const{
+	vector<string> color_array(graph.num_vertices);
+	for (int i=0;i<graph.num_vertices;i++){
+		if (graph.fprops[IS_TERMINAL][i]){
+			color_array[i] = "red";
+		}
+		else{
+			color_array[i] = "black";
+		}
+	}
+	return color_array;
+}
+
+string Node_switching_game::format_action(int action) const{
+	int vertex = action+2;
+	assert (vertex<graph.num_vertices);
+	return to_string(action)+"("+to_string(graph.lprops[BOARD_LOCATION][vertex])+")";
+}
+
+void Node_switching_game::graphviz_me (string fname) const{
+	vector<string> nodetext(graph.num_vertices);
+	std::iota(std::begin(nodetext), std::end(nodetext), 0);
+	graphviz_me(nodetext,fname);
+}
+
+void Node_switching_game::graphviz_me (vector<string> nodetext, string fname) const{
+	vector<string> color_map = get_colors();
+	vector<string> pos_map = get_grid_layout();
+	vector<pair<string,vector<string>>> props;
+	props.push_back(pair<string,vector<string>>("color",color_map));
+	props.push_back(pair<string,vector<string>>("pos",pos_map));
+	props.push_back(pair<string,vector<string>>("label",nodetext));
+
+	graph.graphviz_me(props,fname,true);
+};
+
+std::vector<torch::Tensor> Node_switching_game::convert_graph(torch::Device &device, const Graph& g) const{
+	Neighbors neigh;
+	int n = g.num_vertices;
+	assert(n>0);  // This position is won for some player. Check it first.
+	torch::TensorOptions options_long = torch::TensorOptions().dtype(torch::kLong).device(device);
+	torch::TensorOptions options_float = torch::TensorOptions().dtype(torch::kFloat32).device(device);
+	torch::Tensor node_features = torch::zeros({n,3},options_float);
+	node_features.index_put_({Ellipsis,0},torch::tensor(g.fprops[IS_TERMINAL],options_float));
+
+#ifdef SINGLE_GRAPH
+	if (onturn==RED){
+#else
+	if ((onturn==RED && &g==&graph)||(onturn==BLUE && &g==&graph2)){
+#endif
+		node_features.index_put_({Ellipsis,2},1.);
+	}
+	if (swap_allowed){
+		switch (move_num){
+			case 0:
+				node_features.index_put_({Ellipsis,2},-1.);
+				break;
+			case 1:
+				node_features.index_put_({Ellipsis,2},1.);
+				break;
+			case 2:
+				node_features.index_put_({Ellipsis,2},0.);
+				break;
+		}
+	}
+
+	torch::Tensor edge_index = torch::empty({2,(int)g.sources.size()},options_long);
+	edge_index.index_put_({0,Ellipsis},torch::tensor(g.sources,options_long));
+	edge_index.index_put_({1,Ellipsis},torch::tensor(g.targets,options_long));
+
+	std::vector<torch::Tensor> parts;
+	parts.push_back(node_features);
+	parts.push_back(edge_index);
+	return parts;
+};
+
+std::vector<torch::Tensor> Node_switching_game::convert_graph(torch::Device &device) const{
+#ifdef SINGLE_GRAPH
+	return convert_graph(device,graph);
+#else
+	vector<torch::Tensor> out,parts1,parts2;
+	parts1 = convert_graph(device,graph);
+	parts2 = convert_graph(device,graph2);
+#endif
+};
---
+++ cpp_graph_game/hex_graph_game/shannon_restructured.h
@@ -0,0 +1,109 @@
+#include <torch/script.h>
+#include <torch/csrc/autograd/variable.h>
+#include <torch/csrc/autograd/function.h>
+#include <ATen/ATen.h>
+#include "hex_board_game.h"
+#include "graph.h"
+
+using namespace std;
+using namespace torch::indexing;
+
+#if !defined(SHANNON_H)
+#define SHANNON_H
+
+enum TerminalType {
+    TERMINAL_LOSS,
+    TERMINAL_DRAW,
+    TERMINAL_WIN,
+    TERMINAL_CUSTOM,
+    TERMINAL_NONE
+};
+
+
+enum TerminalID {TERMINAL_1, TERMINAL_2};
+enum Onturn {RED,BLUE,NOPLAYER};
+enum Fprops {IS_TERMINAL};
+enum Lprops {REMOVED,BOARD_LOCATION};
+
+
+bool check_if_same(Graph& graph, int v1, int v2);
+
+bool is_fully_connected(Graph& g,int vert, int ignore);
+
+bool is_fully_connected(Graph& g,Neighbors& neigh, int ignore);
+
+bool is_fully_connected(Graph& g,Neighbors& neigh);
+
+class Node_switching_game {
+	public:
+		Graph graph;
+#ifndef SINGLE_GRAPH
+		Graph graph2;
+#endif
+		Onturn onturn=RED;
+		int board_size;
+		Hex_board board;
+		int move_num;
+		int swap_allowed;
+#ifndef NO_PLAY
+		map<int,int> response_set_red;
+		map<int,int> response_set_blue;
+		vector<int> board_moves_red;
+		vector<int> board_moves_blue;
+#endif
+
+		Node_switching_game (int board_size=11);
+
+		Node_switching_game (std::vector<torch::Tensor> &data);
+
+		Node_switching_game (Hex_board& from_board);
+
+		Node_switching_game(Node_switching_game& ref);
+
+		Node_switching_game * clone();
+
+		void reset();
+
+		uint32_t hash_key() const;
+
+		int vertex_from_board_location(int bl) const;
+
+		int action_from_board_location(int bl) const;
+
+		int get_response(int bloc,bool for_red); // Not const, deletes response
+
+		set<int> fix_terminal_connections(int terminal);
+
+		void remove_marked_nodes();
+
+		void switch_onturn();
+
+		Onturn not_onturn();
+
+		int get_random_action() const;
+
+		vector<int> get_actions() const;
+
+		set<int> make_move(int action, bool do_force_color=false, Onturn force_color=NOPLAYER, bool do_remove_dead_and_captured=false, bool only_mark_removed=false);
+
+		void remove_dead_and_captured(set<int> &consider_set);
+
+		TerminalType get_winner() const;
+
+		Onturn who_won() const;
+
+		string format_action(int action) const;
+
+		vector<string> get_grid_layout() const;
+
+		vector<string> get_colors() const;
+
+		void graphviz_me (string fname) const;
+
+		void graphviz_me (vector<string> nodetext,string fname) const;
+
+		std::vector<torch::Tensor> convert_graph(torch::Device &device) const;
+
+		std::vector<torch::Tensor> convert_graph(torch::Device &device, const Graph& graph) const;
+};
+#endif
